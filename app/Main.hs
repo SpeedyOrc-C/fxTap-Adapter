adf6b82e8493fae934a6e6089f7b66d41de6239c
陈湛明 <chenzhanming04@foxmail.com>
2024-09-30 15:35:07 +0100

    Allow specifying output path

--- app/Main.hs
+++ app/Main.hs
@@ -4,46 +4,52 @@ import Data.ByteString.Lazy qualified as BL
 
 import GHC.IO.Encoding (setLocaleEncoding, utf8)
 import Text.Parsec (parse)
+import Data.Maybe (fromMaybe)
 import Data.Foldable (traverse_)
 import Data.Binary.Put (runPut)
 import Control.Monad (unless)
 import System.Exit (exitFailure, exitSuccess)
-import System.FilePath (takeBaseName, takeDirectory)
+import System.FilePath (dropExtension)
 import System.Environment (getArgs)
 
 import Osu.Parser (pOsu)
 import FxTap ( FxTapCompatible(toFxTap), putFxTap, fxTapPutCheck, FxTapPutMessage (FxTapPutMessage) )
 
-process :: String -> String -> IO ()
-process raw path = case parse pOsu "" raw of
-    Left parseError -> do
-        putStrLn "Bad beatmap."
-        putStrLn "If you believe this is an error, report to us."
-        putStrLn ""
-        print parseError
-        exitFailure
-
-    Right osu -> do
-        let outputPath =
-                takeDirectory path ++ "/" ++
-                takeBaseName path ++ ".fxt"
-
-            fxTap = toFxTap osu
-
-            FxTapPutMessage warnings errors = fxTapPutCheck fxTap
-
-        unless (null warnings) $ do
-            putStrLn "Warning(s)"
-            traverse_ print warnings
+process :: FilePath -> Maybe FilePath -> IO ()
+process inputPath maybeOutputPath = do
+    raw <- readFile inputPath
+    case parse pOsu "" raw of
+        Left parseError -> do
+            putStrLn "Bad beatmap."
+            putStrLn "If you believe this is an error, report to us."
             putStrLn ""
-
-        unless (null errors) $ do
-            putStrLn "Error(s)"
-            traverse_ print errors
+            print parseError
             exitFailure
 
-        BL.writeFile outputPath (runPut $ putFxTap fxTap)
-        exitSuccess
+        Right osu -> do
+            let
+                defaultOutputPath =
+                    dropExtension inputPath ++ ".fxt"
+
+                outputPath = dropExtension
+                    (fromMaybe defaultOutputPath maybeOutputPath) ++ ".fxt"
+
+                fxTap = toFxTap osu
+
+                FxTapPutMessage warnings errors = fxTapPutCheck fxTap
+
+            unless (null warnings) $ do
+                putStrLn "Warning(s)"
+                traverse_ print warnings
+                putStrLn ""
+
+            unless (null errors) $ do
+                putStrLn "Error(s)"
+                traverse_ print errors
+                exitFailure
+
+            BL.writeFile outputPath (runPut $ putFxTap fxTap)
+            exitSuccess
 
 main :: IO ()
 main = do
@@ -51,9 +57,12 @@ main = do
     setLocaleEncoding utf8
 
     case args of
-        [path] -> do
-            raw <- readFile path
-            process raw path
+        [inputPath] ->
+            process inputPath Nothing
+
+        [inputPath, outputFileName] ->
+            process inputPath (Just outputFileName)
+
         _ -> do
             putStrLn "Please provide an osu!mania beatmap."
             exitFailure
