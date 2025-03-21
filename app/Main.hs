89a040d91dc9877de10df2f7aa40fd38c19b45f8
陈湛明 <chenzhanming04@foxmail.com>
2025-03-21 15:09:56 +0000

    Support C header file output

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,7 @@ import GHC.IO.Encoding (setLocaleEncoding, utf8)
 import Text.Parsec (parse)
 import Data.Char (toLower)
 import Data.Maybe (fromMaybe)
-import Data.Foldable (for_)
+import Data.Foldable (for_, traverse_)
 import Data.Binary.Put (runPut)
 import Data.Aeson (decode)
 import Control.Monad (when)
@@ -15,10 +15,12 @@ import System.FilePath (dropExtension, takeExtension)
 import System.Environment (getArgs)
 
 import Data.Beatmap.FxTap ( FxTapCompatible(toFxTap), toFxTap )
-import Data.Beatmap.FxTap.Put ( putFxTap )
+import Data.Beatmap.FxTap.Put ( putFxTapBinary, putFxTapCHeader )
 import Data.Beatmap.FxTap.Checker ( fxTapChecker, runChecker, FxTapMessage (..), isError, Explain (..) )
 import Data.Beatmap.Osu.Parser (parserOsu)
 import Data.Beatmap.Malody (Malody)
+
+data OutputType = OutputBinary | OutputCHeader String
 
 red :: String -> String
 red str = "\x1b[31m" ++ str ++ "\x1b[0m"
@@ -26,20 +28,20 @@ red str = "\x1b[31m" ++ str ++ "\x1b[0m"
 yellow :: String -> String
 yellow str = "\x1b[33m" ++ str ++ "\x1b[0m"
 
-process :: FilePath -> Maybe FilePath -> IO ()
-process inputPath maybeOutputPath = do
+rock :: OutputType -> FilePath -> Maybe FilePath -> IO ()
+rock outputType inputPath maybeOutputPath = do
     result <- case map toLower (takeExtension inputPath) of
         ".osu" -> do
             raw <- readFile inputPath
-            case parse parserOsu "" raw of
-                Left error' -> return $ Left (show error')
-                Right osu -> return $ Right (toFxTap osu)
+            return $ case parse parserOsu "" raw of
+                Left error' -> Left (show error')
+                Right osu -> Right (toFxTap osu)
 
         ".mc" -> do
             raw <- BL.readFile inputPath
-            case decode raw :: Maybe Malody of
-                Nothing -> return $ Left "Cannot parse this Malody beatmap."
-                Just malody -> return $ Right (toFxTap malody)
+            return $ case decode raw :: Maybe Malody of
+                Nothing -> Left "Cannot parse this Malody beatmap."
+                Just malody -> Right (toFxTap malody)
 
         "" -> return $ Left "No extension found, cannot determine the file type."
 
@@ -55,8 +57,12 @@ process inputPath maybeOutputPath = do
             exitFailure
 
         Right fxTap -> do
-            let defaultOutputPath = dropExtension inputPath ++ ".fxt"
-            let outputPath = dropExtension (fromMaybe defaultOutputPath maybeOutputPath) ++ ".fxt"
+            let extension = case outputType of
+                    OutputBinary -> ".fxt"
+                    OutputCHeader {} -> ".fxt.h"
+
+            let defaultOutputPath = dropExtension inputPath
+            let outputPath = dropExtension (fromMaybe defaultOutputPath maybeOutputPath) ++ extension
             let messages = runChecker fxTapChecker fxTap
 
             for_ messages $ \message -> do
@@ -67,11 +73,29 @@ process inputPath maybeOutputPath = do
 
             when (any isError messages) exitFailure
 
-            BL.writeFile outputPath (runPut $ putFxTap fxTap)
+            BL.writeFile outputPath . runPut $ case outputType of
+                OutputBinary -> putFxTapBinary fxTap
+                OutputCHeader symbolName -> putFxTapCHeader symbolName fxTap
+
             exitSuccess
 
 printVersion :: IO ()
-printVersion = putStrLn "fxTap Adapter 0.4.0.1"
+printVersion = putStrLn "fxTap Adapter 0.5.0.0"
+
+printHelp :: IO ()
+printHelp =
+    traverse_ putStrLn
+    [ "Usage: fxta [-ch <symbol_name>] <beatmap_path> [<output_path>]"
+    , ""
+    , "beatmap_path : Path to osu! or Malody beatmap."
+    , "output_path : Path to the converted beatmap. If omitted, it is the same as <beatmap_path>."
+    , "symbol_name : If provided, the output will be a C header file, with a initialised variable called <symbol_name>."
+    ]
+
+printInvalidArguments :: IO ()
+printInvalidArguments = do
+    putStrLn "Invalid arguments. Please use -h or --help for more information."
+    exitFailure
 
 main :: IO ()
 main = do
@@ -81,13 +105,19 @@ main = do
     case args of
         ["-v"] -> printVersion
         ["--version"] -> printVersion
+        ["-h"] -> printHelp
+        ["--help"] -> printHelp
+
+        ["-ch", symbolName, inputPath] ->
+            rock (OutputCHeader symbolName) inputPath Nothing
+
+        ["-ch", symbolName, inputPath, outputFileName] ->
+            rock (OutputCHeader symbolName) inputPath (Just outputFileName)
 
         [inputPath] ->
-            process inputPath Nothing
+            rock OutputBinary inputPath Nothing
 
         [inputPath, outputFileName] ->
-            process inputPath (Just outputFileName)
+            rock OutputBinary inputPath (Just outputFileName)
 
-        _ -> do
-            putStrLn "Please provide an beatmap."
-            exitFailure
+        _ -> printInvalidArguments
